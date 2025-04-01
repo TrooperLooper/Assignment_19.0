02bc9892c13d598869e6d242d1a6d0582a8181d0
Lars <lars@recap.dk>
2025-04-01 08:09:33 +0200

    latest commit
    Did first, second ass. third was a bit harder to sole so i need help.

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
+/*
+// ------------------------ Uppgift 1 ---------------------------------
 
 
-const target = Math.floor(Math.random()*10);
+const target = Math.floor(Math.random() * 10)+1;
 let guess = null;
 
-while (guess !== target) {
-    console.log(`target : ${target} Guess: ${guess}`);
 guess = parseFloat(prompt("Gissa en siffra mellan 1 och 10!"));
-} if ${guess} 
 
-console.log("högre!");
-console.log("lägre!");
+while (guess !== target) {
+        if (false) {
+            console.log(`target : ${target} guess: ${guess}`);
+        }
+    guess = parseFloat(prompt("Gissa en siffra mellan 1 och 10!"));
+        
+     if(guess > target) {
+        console.log("nej, lägre!");
+        
+    } else if (guess < target) {
+        console.log("näpp, högre!");
+    }
+}
 
-if(guess === target)
-console.log(`target : ${target} Guess: ${guess}`);
-console.log("Slay!");
+console.log(`Grattis! Nu gissade du rätt! Siffran var: ${target}`);           
+  
+// ------------------------ Uppgift 2 ---------------------------------
 
 
 
 
+let a = []
+for ( i = 0; i < 100; ++i) a[i] = i;
+
+function randomizer () {
+    console.log(Array.from({
+        length: 10
+    }, () => Math.floor(Math.random() * 100) +1 ));
+}
+randomizer ()
+
+
+/*
+
+
+// ------------------------ Uppgift 3 ---------------------------------
+
+
+
+let arr [i]
+      
+function sumEvenNumbers (arr)% 2; {
+    if arr (i % 2 {
+        for ( i = 0; i < 10; ++i) a[i] = i;
+    }
+
+    while sumEvenNumbers är jämna, addera summa
+´
+}
+
+return summa
+   
+
+--------------------
 /*
 // EXERCISE 1: Guess the random number 
 
@@ -52,4 +95,4 @@ Create a function sumEvenNumbers that accepts an array as an argument.
 Use a for loop to iterate through the array and calculate the sum of the even numbers. 
 
 Return the sum of the even numbers. 
-/*
+*/
